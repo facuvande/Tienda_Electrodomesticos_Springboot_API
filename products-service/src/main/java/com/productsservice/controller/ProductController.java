1b24c59c0969f8409306915d2f112d6604d96bc8
Facundo <facundovs84@hotmail.com.ar>
2024-02-17 15:29:48 -0300

    Implementacion de metodos http en ProductController

--- products-service/src/main/java/com/productsservice/controller/ProductController.java
+++ products-service/src/main/java/com/productsservice/controller/ProductController.java
@@ -1,12 +1,48 @@
 package com.productsservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.productsservice.model.Product;
+import com.productsservice.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    
+    @Autowired
+    private IProductService productService;
 
+    @GetMapping("")
+    public List<Product> getProducts(){
+        return productService.getProducts();
+    }
+
+    @GetMapping("/{id_product}")
+    public ResponseEntity<?> getProductById(@PathVariable Long id_product){
+        Product myProduct = productService.getProductById(id_product);
+        if(myProduct != null){
+            return ResponseEntity.ok(myProduct);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+    }
+
+    @PostMapping("")
+    public Product createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+
+    @DeleteMapping("/{id_product}")
+    public String deleteProduct(@PathVariable Long id_product){
+        return productService.deleteProductById(id_product);
+    }
+
+    @PutMapping("/{id_product}")
+    public Product editProduct(@PathVariable Long id, @RequestBody Product newProduct){
+        return productService.editProduct(id, newProduct);
+    }
 }
