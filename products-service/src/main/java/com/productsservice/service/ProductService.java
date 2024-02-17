eb79178c05cdc1e4692af82abbe353e18f6ff446
Facundo <facundovs84@hotmail.com.ar>
2024-02-17 13:16:47 -0300

    Implementacion de metodos de productos

--- products-service/src/main/java/com/productsservice/service/ProductService.java
+++ products-service/src/main/java/com/productsservice/service/ProductService.java
@@ -1,34 +1,48 @@
 package com.productsservice.service;
 
 import com.productsservice.model.Product;
+import com.productsservice.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService implements IProductService{
+
+    @Autowired
+    private IProductRepository productRepository;
+
     @Override
     public List<Product> getProducts() {
-        return null;
+        return productRepository.findAll();
     }
 
     @Override
     public Product getProductById(Long id) {
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 
     @Override
     public Product createProduct(Product product) {
-        return null;
+        return productRepository.save(product);
     }
 
     @Override
     public String deleteProductById(Long id) {
-        return null;
+        productRepository.deleteById(id);
+        return "Product Deleted";
     }
 
     @Override
     public Product editProduct(Long id, Product newProduct) {
+        Product myProduct = this.getProductById(id);
+        if(myProduct != null) {
+            myProduct.setBrand(newProduct.getBrand());
+            myProduct.setName(newProduct.getName());
+            myProduct.setUnit_price(newProduct.getUnit_price());
+            productRepository.save(myProduct);
+        }
         return null;
     }
 }
