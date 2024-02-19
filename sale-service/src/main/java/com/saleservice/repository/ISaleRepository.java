e0658a4e700763bacda6f9721b93e535d12cf1c7
Facundo <facundovs84@hotmail.com.ar>
2024-02-19 15:46:02 -0300

    Creacion de SaleRepository

---
+++ sale-service/src/main/java/com/saleservice/repository/ISaleRepository.java
@@ -0,0 +1,9 @@
+package com.saleservice.repository;
+
+import com.saleservice.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISaleRepository extends JpaRepository<Sale, Long> {
+}
