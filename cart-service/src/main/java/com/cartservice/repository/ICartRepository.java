d53852101f452557eb79054021553b36d4b5c171
Facundo <facundovs84@hotmail.com.ar>
2024-02-18 21:20:15 -0300

    Agregado mapeo de repository

--- cart-service/src/main/java/com/cartservice/repository/ICartRepository.java
+++ cart-service/src/main/java/com/cartservice/repository/ICartRepository.java
@@ -2,6 +2,8 @@ package com.cartservice.repository;
 
 import com.cartservice.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ICartRepository extends JpaRepository<Cart, Long> {
 }
